62ed7e52bdbb98b141af5ca2132e051d1ac3a46f
anand <anand.solucky@gmail.com>
2021-01-01 14:53:43 +0530

    Test One - passed
    added more tests in test one
    added basic add functionality

--- TDD-StringCalculator/src/StringCalculator.java
+++ TDD-StringCalculator/src/StringCalculator.java
@@ -1,6 +1,13 @@
 
 public class StringCalculator {
 	int add(String numbers) {
-		return 1; 
+		if(numbers == "") return 0;
+		else {
+			String tokens[] = numbers.split(",");
+			if(tokens.length < 2) return 0;
+			else {
+				return Integer.parseInt(tokens[0]) + Integer.parseInt(tokens[1]);
+			}
+		}
 	}
 }
--- TDD-StringCalculator/src/StringCalculatorTest.java
+++ TDD-StringCalculator/src/StringCalculatorTest.java
@@ -14,6 +14,11 @@ class StringCalculatorTest {
 	void testOneSimpleAdd() {
 		int expected = 2;
 		assertEquals(expected, sc.add("1,1"), "Simple add operation failed");
-		
+		expected = 4;
+		assertEquals(expected, sc.add("2,2"), "Simple add operation failed");
+		expected = 5;
+		assertEquals(expected, sc.add("3,2"), "Simple add operation failed");
+		expected = 0;
+		assertEquals(expected, sc.add(""), "Simple add operation failed");
 	}
 }
